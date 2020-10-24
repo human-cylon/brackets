6c79461c9f1571a1d89216f3ecf331be31335642
Alina Soboleva <alina.soboleva.spb@gmail.com>
2020-10-25 02:37:26 +0300

    feat: add function check if brackets are balanced

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let len = bracketsConfig.length;
+  let brackets = "";
+ 
+  for (let i = 0; i < len; i++) {
+    brackets += bracketsConfig[i].join("");
+  }
+
+  console.log(brackets);
+    
+  for (let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+
+    if (bracketsIndex % 2 === 0) {
+        if (brackets[bracketsIndex] === 
+        brackets[bracketsIndex + 1] &&
+        stack[stack.length - 1] === bracketsIndex + 1) {stack.pop();}
+        else {stack.push(bracketsIndex + 1);}
+
+        console.log(stack);
+    }
+    else {
+       if (stack.pop() !== bracketsIndex) {return false;}
+    }
+  }
+  console.log(stack);
+  return stack.length === 0;
 }
